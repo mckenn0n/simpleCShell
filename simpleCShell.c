dea0523ebf4df98c16e56e1a5877690c4115f226
McKennon McMillian <mbmcmill@go.olemiss.edu>
2018-02-26 03:58:12 -0600

    needs clean up

--- simpleCShell.c
+++ simpleCShell.c
@@ -3,15 +3,21 @@
 #include <string.h>
 #define MAX_LINE 80 
 
-typedef struct{
-	int N;
-	char *cmd;
-}CMD;
+// typedef struct{
+// 	int n;
+// 	char *cmd;
+// }CMD;
 
 int main(void){
 
 	char *args[MAX_LINE/2 + 1]; 
 	int should_run = 1; 
+	// CMD *hist[10];
+	int n[10];
+	char cmd[10][MAX_LINE];
+	int histC = 0;
+	int c = 0;
+	int count = 1;
 
 	while (should_run) {
 		// sleep(1);
@@ -23,7 +29,40 @@ int main(void){
 
 	    fgets(input,sizeof(input),stdin);
 	    input[strlen(input)-1] = '\0';
-
+	    if(input[0]=='!'){
+	    	if(input[1]=='!'){
+	    		if(c==0){
+	    			printf("%s\n", "No commands in history");
+	    			strcpy(input,"history"); 
+	    			count--;
+	    		}else{
+		    		int index = (count % 10) - 2;
+		    		count--;
+		    		printf("%s\n", cmd[index]);
+		    		strcpy(input,cmd[index]); 
+		    	}
+	    	}else{
+	    		int num = input[1] - '0';
+	    		int index = (num % 10) - 1;
+	    		// input = cmd[index];
+	    		printf("%s\n",cmd[index]);
+	    		strcpy(input,cmd[index]); 
+	    		count--;
+	    	}
+	    	count++;
+	    }
+	    if(strcmp(input,"history")){
+		    if(histC == 10){
+		    	histC = 0;
+		    }
+		    n[histC] = count;
+		    count++;
+		    strcpy(cmd[histC], input);
+		    histC++;
+		    if(c != 10){
+		    	c++;
+		    }
+		}
 	    const char d[2] = " ";
 	    char *token;
 
@@ -47,6 +86,14 @@ int main(void){
 
 	    if(!strcmp(args[0],"exit")){
 	    	should_run = 0;
+	    }
+	    if(!strcmp(args[0],"history")){
+	    	int x = c;
+	    	for(;x >= 0; x--){
+	    		if(n[x] != 0){
+		    		printf("%d  %s\n",n[x], cmd[x]);
+	    		}
+		    }
 	    }
 
 	    int counter = 0;
